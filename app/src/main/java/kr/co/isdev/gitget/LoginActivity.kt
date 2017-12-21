31e36040d3ca22de7f1a4e4882e3956601dc104d
Jang Insu(word) <isjang98@gmail.com>
2017-12-21 23:09:44 +0900

    LoginActivity  - Add LoginActivity  - Add activity_login.xml

---
+++ app/src/main/java/kr/co/isdev/gitget/LoginActivity.kt
@@ -0,0 +1,16 @@
+package kr.co.isdev.gitget
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by word on 21/12/2017.
+ */
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
