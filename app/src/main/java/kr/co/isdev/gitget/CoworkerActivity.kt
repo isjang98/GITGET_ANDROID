9fb9ef596e609386c97fcce9867384019bdaf970
Jang Insu(word) <isjang98@gmail.com>
2018-02-24 00:36:03 +0900

    Activity 추가
    - CoworkerActivity 추가

---
+++ app/src/main/java/kr/co/isdev/gitget/CoworkerActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.isdev.gitget
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class CoworkerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coworker)
+    }
+}
